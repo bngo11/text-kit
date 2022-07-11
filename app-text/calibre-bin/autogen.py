b039443bc6bfb7b38566bbe854938c180f7b5828
shadow <shadowngoz@gmail.com>
2022-07-11 13:59:37 -0500

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: e3b28797d7f10db96997af557b88b2a06d8743d3

--- app-text/calibre-bin/autogen.py
+++ app-text/calibre-bin/autogen.py
@@ -3,6 +3,7 @@
 from bs4 import BeautifulSoup
 
 async def generate(hub, **pkginfo):
+	url = None
 	major_url = f"http://download.calibre-ebook.com/"
 	major_data = await hub.pkgtools.fetch.get_page(major_url)
 	major_soup = BeautifulSoup(major_data, "html.parser")
@@ -18,19 +19,17 @@ async def generate(hub, **pkginfo):
 	
 	for link in src_soup.find_all("a"):
 		href = link.get("href")
-		if href is not None and href.endswith("i686.txz"):
+		if href is not None and href.endswith("x86_64.txz"):
 			url = src_url + href
 			break
 
-	ebuild = hub.pkgtools.ebuild.BreezyBuild(
-		**pkginfo,
-		version=version,
-		artifacts={
-			"x86": hub.pkgtools.ebuild.Artifact(url=url),
-			"amd64": hub.pkgtools.ebuild.Artifact(url=url.replace("i686","x86_64")),
-		}
-	)
-	ebuild.push()
+	if url and version:
+		ebuild = hub.pkgtools.ebuild.BreezyBuild(
+			**pkginfo,
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url)],
+		)
+		ebuild.push()
 
 
 # vim: ts=4 sw=4 noet
