b0824b9154ecf19684eb91f940a96719409604ca
shadow <shadowngoz@gmail.com>
2025-02-17 23:26:16 -0600

    Autogenerated tree updates.
    merged:
    
      flora: 523a446b976b43c4ede7fd49048643aaf4812e42
      gentoo-staging: 9292583023a6610905c79a4707db3ac6c63b3a31
      gentoo-staging-nov-2021: a15a64f48741a493e49dfee7d4280a3dc10cd21d
      kit-fixups: 91f4a15b386e744adda0f180e7ba7f1c6636f8f1

--- app-text/mandoc/autogen.py
+++ app-text/mandoc/autogen.py
@@ -1,68 +1,36 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
-from metatools.version import generic
-
-def get_version_from_href(href, name, extension):
-	file = href.split("/")[-1]
-	return file[len(name)+1:-len(extension)]
-
-def filter_valid_href(href, name, extension):
-	"""
-	Assuming ``name`` is "gzip" and ``extension`` is ".tar.gz", this will look at ``href``, a URL or file, and
-	return True if it points to a file starting with "gzip-" and ending with ".tar.gz". This is used on
-	directory listings to validate that the URLs we are looking at appear to be pointing to the tarball
-	we care about. This function is to be used with ``filter()``.
-	"""
-	if not href.endswith(extension):
-		return False
-	if not href.split("/")[-1].startswith(f"{name}-"):
-		return False
-	return True
-
-def filter_and_sort_hrefs(hrefs, name, extension):
-	"""
-	Given a list of URLs or files from a directory listing (``hrefs``), this function will return a list of tuples.
-	The tuples will consist of the original URL/file string, along with the extracted version, and the list will be
-	sorted (from least to greatest) by version using generic.parse. For example::
-
-	  [ ( "gzip-1.0.tar.gz", "1.0" ), ( "gzip-2.0.tar.gz" , "2.0" ) ]
-
-	Note that any bogus files or URLS -- those not starting with ``name`` or ending with ``extension`` -- thus pointing
-	to things we don't care about -- will be removed from the output.
-	"""
-	valid_list = filter(lambda href: filter_valid_href(href, name, extension), hrefs)
-	tuple_list = map(lambda href: (href, get_version_from_href(href, name, extension)), valid_list)
-	sorted_list = sorted(tuple_list, key=lambda tup: generic.parse(tup[1]))
-	return list(sorted_list)
 
 async def generate(hub, **pkginfo):
-	app = "mandoc"
-	extension = f".tar.gz"
-	src_url = f"https://mandoc.bsd.lv/snapshots/"
-	src_data = await hub.pkgtools.fetch.get_page(src_url)
-	soup = BeautifulSoup(src_data, "html.parser")
+	html_data = await hub.pkgtools.fetch.get_page("https://mandoc.bsd.lv/")
+	soup = BeautifulSoup(html_data, "html.parser")
+	links = soup.find_all("p")
+	version = None
 
-	hrefs = []
+	for link in links:
+		text = link.get_text().split("\n")
+		for parts in text:
+			if "current version" in parts:
+				for version in parts.split(" "):
+					try:
+						list(map(int, version.split(".")))
+						break
 
-	for link in soup.find_all("a"):
-		hrefs.append(link.get("href"))
-	href_tuples = filter_and_sort_hrefs(hrefs, app, extension)
+					except ValueError:
+						continue
+		if version:
+			break
 
-	if not len(href_tuples):
-		raise hub.pkgtools.ebuild.BreezyError(f"No valid tarballs found for {app}.")
-	href, version = href_tuples[-1]
-	artifact = hub.pkgtools.ebuild.Artifact(url=f"{src_url}{href}")
-	generate = []
-	generate.append({ "cat" : "app-text", "name": "mandoc", "version" : version})
-
-	for gen in generate:
-		pkginfo.update(gen)
+	if version:
+		final_name = f"{pkginfo.get('name')}-{version}.tar.gz"
+		url = f"https://mandoc.bsd.lv/snapshots/{final_name}"
 		ebuild = hub.pkgtools.ebuild.BreezyBuild(
 			**pkginfo,
-			artifacts=[artifact],
-			template=pkginfo['name']+".tmpl"
+			version=version,
+			artifacts=[hub.pkgtools.ebuild.Artifact(url=url, final_name=final_name)],
 		)
+
 		ebuild.push()
 
 
--- app-text/mdbook/mdbook-0.4.45.ebuild
+++ app-text/mdbook/mdbook-0.4.45.ebuild
@@ -6,9 +6,9 @@ inherit cargo
 
 DESCRIPTION="Create a book from markdown files"
 HOMEPAGE="https://rust-lang.github.io/mdBook/"
-SRC_URI="https://github.com/rust-lang/mdBook/tarball/40d91fff29af051a400c866d53653d69b7529298 -> mdBook-0.4.44-40d91ff.tar.gz
-https://direct.funtoo.org/7a/84/92/7a8492e9ced0fca4a4e626d257eee194ed3825c90946dc4a52c809525a742e981ab6e96df7291ffde52126ecc0e45cd207b909700affe559983782fb5c6df69e -> mdbook-0.4.44-funtoo-crates-bundle-4a4b465c481e86d55c6ccc6c98b4a3725d0f11fe32175c4e55464b4159229c1073efa138ec30d68fe4fb341cc2f964d074ee92badf1cc7c37e98529ef6b725a6.tar.gz"
-S="${WORKDIR}/rust-lang-mdBook-40d91ff"
+SRC_URI="https://github.com/rust-lang/mdBook/tarball/4941acdb8705728a5ea9d77987f31fdafefdcee1 -> mdBook-0.4.45-4941acd.tar.gz
+https://direct.funtoo.org/4c/fc/96/4cfc96e7005b3c76983167efe5b86774907d142c3bf533f7bdd20248ef3d9ce5295a4763b6b0d2e4133a41bfb75e04652e8a2dc0403ff4ba83465305fc95c019 -> mdbook-0.4.45-funtoo-crates-bundle-4a4b465c481e86d55c6ccc6c98b4a3725d0f11fe32175c4e55464b4159229c1073efa138ec30d68fe4fb341cc2f964d074ee92badf1cc7c37e98529ef6b725a6.tar.gz"
+S="${WORKDIR}/rust-lang-mdBook-4941acd"
 
 # CC-BY-4.0/OFL-1.1: embeds fonts inside the executable
 LICENSE="MPL-2.0 CC-BY-4.0 OFL-1.1"
